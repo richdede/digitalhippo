431341c8d0ee7a62cc59905e64a5afdd1ad13379
richdede <deeprince2002@gmail.com>
2024-01-19 10:51:21 -0800

    changes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digitalhippo-production-24a6.up.railway.app",
-			},
+		domains: [
+			"localhost",
+			"digitalhippo-production-24a6.up.railway.app",
 		],
 	},
 };
